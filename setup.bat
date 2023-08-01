8ff7f97a3c1f8b533da39fccae42ffaaf0b20193
Tarun <tarunsrisaiv03@gmail.com>
2023-08-01 09:48:56 +0530

    Install angular cli

--- setup.bat
+++ setup.bat
@@ -1,3 +1,3 @@
 @echo OFF
 start cmd /k "pip install pandas scikit-learn flask flask-cors & exit"
-start cmd /k "cd my-ai & npm install & exit"
+start cmd /k "cd my-ai & npm install & npm install @angular/cli & npm audit fix --force & exit"
