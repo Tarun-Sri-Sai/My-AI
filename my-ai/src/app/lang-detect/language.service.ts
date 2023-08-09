3ba58b46ea352278d9ee683ac26af754e3996233
Tarun <tarunsrisaiv03@gmail.com>
2023-08-09 16:31:52 +0530

    Indent to 4 spaces

--- my-ai/src/app/lang-detect/language.service.ts
+++ my-ai/src/app/lang-detect/language.service.ts
@@ -2,46 +2,46 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class LanguageService {
-  language: string = '';
-  private timeout: boolean = false;
-  private timeoutMillis: number = 500;
+    language: string = '';
+    private timeout: boolean = false;
+    private timeoutMillis: number = 500;
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {}
 
-  detectLanguage(textInput: string): void {
-    if (this.timeout) {
-      return;
+    detectLanguage(textInput: string): void {
+        if (this.timeout) {
+            return;
+        }
+        this.timeout = true;
+        this.makeRequest(textInput);
+        this.resetTimeout();
     }
-    this.timeout = true;
-    this.makeRequest(textInput);
-    this.resetTimeout();
-  }
 
-  makeRequest(textInput: string): void {
-    this.http
-      .post<any>('http://localhost:5000/language_detector/language', {
-        text_input: textInput,
-      })
-      .subscribe({
-        next: (response) => {
-          this.language = response['language_code'];
-        },
-        error: (error) => {
-          console.error("Couldn't compute language due to: ", error);
-        },
-      });
-  }
+    makeRequest(textInput: string): void {
+        this.http
+            .post<any>('http://localhost:5000/language_detector/language', {
+                text_input: textInput,
+            })
+            .subscribe({
+                next: (response) => {
+                    this.language = response['language_code'];
+                },
+                error: (error) => {
+                    console.error("Couldn't compute language due to: ", error);
+                },
+            });
+    }
 
-  resetTimeout(): void {
-    setTimeout(() => {
-      this.timeout = false;
-    }, this.timeoutMillis);
-  }
+    resetTimeout(): void {
+        setTimeout(() => {
+            this.timeout = false;
+        }, this.timeoutMillis);
+    }
 
-  getLanguage(): string {
-    return this.language;
-  }
+    getLanguage(): string {
+        return this.language;
+    }
 }
